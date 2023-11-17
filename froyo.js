b47e8a0c8d1d41632cd7246ed9487ef1df6dd006
Parker Munson <parkermunson@Parkers-MacBook-Air.local>
2023-11-17 13:50:38 -0500

    First Commit

---
+++ froyo.js
@@ -0,0 +1,5 @@
+const flavors = window.prompt("enter some flavors", "vanilla,vanilla,vanilla,strawberry,coffee,coffee");
+console.log(flavors);
+
+const flavorsArr = flavors.split(',');
+console.log(flavorsArr);
